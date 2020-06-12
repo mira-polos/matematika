3228cf5ed45bb3f612cb3b6a372d4eb3a3815c6d
Mira polos <66188045+mira-polos@users.noreply.github.com>
2020-06-12 10:39:39 +0700

    Hai gais
    hai gais hai guys hai gas wkwkwkwlwkkwkw

---
+++ matematika.py
@@ -0,0 +1,28 @@
+#tools pembantu menghitung
+
+import os
+
+os.system('clear')
+
+banner = """
+
+     >>> welcome to tools mira tersakiti om dan tante <<<
+"""
+print banner
+
+x = input("angka 1: ")
+y = input("angka 2: ")
+
+
+tambah = x + y
+print "hasil dari %d + %d = %d" % (x,y,tambah)
+
+kurang = x - y
+print "hasil dari %d - %d = %d" % (x,y,kurang)
+
+kali   = x * y
+print "hasil dari %d * %d = %d" % (x,y,kali)
+
+bagi   = x / y
+print "hasil dari %d / %d = %d" % (x,y,bagi)
+
